4df4012772000f9789f8ef516f75910cba647790
Alfie Edgeworth <0xalfie@gmail.com>
2023-02-25 15:42:06 +1030

    2023 redesign

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,18 @@
 import '../styles/globals.scss'
 
 import type { AppProps } from 'next/app'
-import { Sidebar } from '../src/Sidebar'
-import { Page } from '../src/Page'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>alfie.codes</title>
         <meta property="og:title" content="alfie.codes" key="title" />
       </Head>
-      <Sidebar />
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <Component {...pageProps} />
     </>
   )
 }
 
-export default MyApp
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,50 @@
 import type { NextPage } from 'next'
-import { Project } from '../src/Project'
+import Link from 'next/link'
+import Image from 'next/image'
 
-import styles from '../src/index.module.scss'
+import styles from './index.module.scss'
 
-const Home: NextPage<ApolloResponse> = ( props ) => {
-  const projects = props.projects.projects
+const Home: NextPage = ( props ) => {
 
   return (
-    <div className={styles.index}>
-      <div className={styles.container}>
-        <div className={styles.projects}>
-          {projects.map(project => (
-            <Project key={project.title} project={project} />
-          ))}
-        </div>
+    <div className={styles.container}>
+      <div className={styles.bio}>
+        <b>Alfie Edgeworth, B. CS</b>
+        <span className={styles.location}>Adelaide, Australia</span>
+        <p>UI Engineer & Designer</p>
+        <Nav 
+          links={[
+            {href: 'mailto:hey@alfie.codes', title: 'Mail'},
+            {href: 'https://read.cv/alfie', title: 'CV'}
+          ]}
+        />
       </div>
-      
+      <div className={styles.tiptap}>
+          <Image layout="fill" src={'/desk.png'} alt={'An illustrated Alfie sits at a desk on his laptop.'}/>
+      </div>
+      <footer>
+        <span>© Alfie Edgeworth 2023</span>
+        <Nav 
+          links={[
+            {href: 'https://github.com/owfie', title: 'GitHub'},
+            {href: 'https://twitter.com/alfie_codes', title: 'Twitter'},
+            {href: 'https://www.linkedin.com/in/alfie-edgeworth/', title: 'LinkedIn'},
+            {href: 'https://dribbble.com/owfie/', title: 'Dribbble'},
+          ]}
+        />
+      </footer>
     </div>
   )
 }
 
-import { gql } from '@apollo/client'
-import client from '../apollo/apollo-client'
-
-type ApolloResponse = {
-  projects: {
-    projects: Project[]
-  }
+interface INav {
+  links: {href: string, title: string}[]
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await client.query<ApolloResponse>({
-    query: gql`
-      query MyQuery {
-        projects(orderBy: publishedAt_ASC) {
-          id
-          name
-          image {
-            url
-          }
-        }
-      }
-    `
-  })
-
-  return {
-    props: {
-      projects: data
-    }
-  }
-}
+const Nav: React.FC<INav> = (props) => <nav className={styles.nav}>
+  {props.links.map((l, i) => 
+    <Link key={`${l.title}-${i}`} href={l.href}>{l.title}</Link>
+  )}
+</nav>
 
 export default Home
