82e75b5378e9e9d392cc6466e76478f558e7f774
Alfie Edgeworth <0xalfie@gmail.com>
2024-01-01 23:49:05 +1030

    add mdx

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
-  images: {
-    domains: ['media.graphassets.com'],
-  },
+  // Optionally, add any other Next.js config below
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
